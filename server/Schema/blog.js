7690e59b8d44ed283498872902f2308ba88a597e
manaswini <manaswini0623@gmail.com>
2025-01-19 16:08:09 +0530

    backend/feature/schema

---
+++ server/Schema/blog.js
@@ -0,0 +1,38 @@
+const blogSchema = new mongoose.Schema({
+    authorId: {
+        type: mongoose.Schema.Types.ObjectId, ref: 'User',
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    likes: {
+        type: Number,
+        default: 0
+    },
+    comments: [
+    {
+          userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+          },
+          comment: {
+              type: String
+          },
+          date: {
+              type: Date, default: Date.now
+          },
+    },
+  ],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+});
+
+module.exports = mongoose.model('Blog', blogSchema);
---
+++ server/Schema/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String, required: true
+    },
+    email: {
+        type: String, required: true, unique: true
+    },
+    password: {
+        type: String, required: true
+    },
+    age: {
+        type: Number
+    },
+    gender: {
+        type: String, enum: ['Male', 'Female', 'Other']
+    },
+    weight: {
+        type: Number
+    },
+    height: {
+        type: Number
+    },
+    profileImage: {
+        type: String
+    },
+    createdAt: {
+        type: Date, default: Date.now
+    },
+});
+
+module.exports = mongoose.model('User', userSchema);
