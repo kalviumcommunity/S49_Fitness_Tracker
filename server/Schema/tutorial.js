f5ad60a6bdf1821160716ac51701c9dbf9f7b028
manaswini <manaswini0623@gmail.com>
2025-01-20 16:00:44 +0530

    Created get request 4/4

--- server/Schema/tutorial.js
+++ server/Schema/tutorial.js
@@ -7,11 +7,14 @@ const tutorialSchema = new mongoose.Schema({
         type: String, required: true
     },
     description: {
-        type: String
+        type: String, enum: ['With Equipment', 'Without Equipment'], required: true
     },
     category: {
         type: String, enum: ['Beginner', 'Intermediate', 'Advanced'], required: true
     },
-});
+    type: {
+        type: String, enum: ['Strength', 'Flexibility', 'Yoga', 'Pilates'], required: true
+    }
+}, {collection: 'Tutorials'});
 
 module.exports = mongoose.model('Tutorial', tutorialSchema);
